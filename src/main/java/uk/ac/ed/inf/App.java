d143a6ac50e39353b96abfd3c081938e5140062f
mohammedAbubaker <m.abubaker1@hotmail.com>
2023-11-15 19:27:33 +0000

    initial

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -8,6 +8,7 @@ import uk.ac.ed.inf.ilp.data.Order;
  */
 public class App 
 {
+    Order oder = new Order();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
