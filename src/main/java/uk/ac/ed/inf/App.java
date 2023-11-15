05ac1377c7661d61450ae6cc210afefd04d34e6c
mohammedAbubaker <m.abubaker1@hotmail.com>
2023-11-15 19:23:08 +0000

    initial

---
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -0,0 +1,15 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.data.Order;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
